1efb379a75ae5eb3662651968d83ebc341c7bcdb
adrielrengel <adrielrengel@gmail.com>
2016-08-12 21:51:45 -0300

    #3 Criado classes 	Venda 	Entrega

---
+++ src/main/java/model/Entrega.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.io.Serializable;
+
+public class Entrega implements Serializable{
+	
+	private String endereco;
+	
+
+}
---
+++ src/main/java/model/Venda.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Venda {
+	
+	private String  produto; //com get voce pode adicionar paranaues sem alterar
+	private String valor;
+}
